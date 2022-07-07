92a69652711818d2714212c0e8de77eeba4b9ee8
GrishaginEvgeny <thenoydota@gmail.com>
2022-07-07 22:37:03 +0300

    fix auth for user wtih uuid

--- migrations/Version20220707184029.php
+++ migrations/Version20220707184029.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220707110846 extends AbstractMigration
+final class Version20220707184029 extends AbstractMigration
 {
     public function getDescription(): string
     {
