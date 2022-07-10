432306abb12129740c74b853a4e10623a1cdc24d
Danila <dendanden74@gmail.com>
2022-07-10 13:20:37 +0300

    Добавление пагинации, починка регистрации

--- migrations/Version20220710061200.php
+++ migrations/Version20220710061200.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220709161326 extends AbstractMigration
+final class Version20220710061200 extends AbstractMigration
 {
     public function getDescription(): string
     {
